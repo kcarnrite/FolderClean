075bd0ebec8258c4e4a7c7748310ea6973518d05
Andreweja <adama5@mcmaster.ca>
2021-01-12 16:03:49 -0500

    Added main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Name: Kyle Carnrite
+# Date Started: January 12th
+# Description: Takes target folder and deletes compressed folders
+# WARNING: Do not use on folders you have important data. This program simply targets compressed folders
+
+
